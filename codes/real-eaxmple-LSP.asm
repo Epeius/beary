8637eefdf66e8ca0fd53919094aa4ce067904ed0
ZhangBin <binzh4ng@hotmail.com>
2017-05-09 11:09:23 +0200

    Init Commit

---
+++ codes/real-eaxmple-LSP.asm
@@ -0,0 +1,13 @@
+loc_805147D:
+	mov	eax, [ebp+i] 
+	mov	edx, [ebp+buf]
+	add	eax, edx
+	movzx	eax, byte ptr [eax]
+	movzx	eax, al
+	movzx	eax, text_chars[eax]
+	movsx	eax, al
+	mov	[ebp+t], eax
+	cmp	[ebp+t], 1
+	jz	short loc_80514A5
+	; do other jobs
+
